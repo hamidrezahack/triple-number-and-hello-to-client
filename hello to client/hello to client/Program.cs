c74cc91bafa3a5efb9dc624b519ab6bc374bb2c9
Hamid reza ghafariannasab <57498756+hamidrezahack@users.noreply.github.com>
2023-05-26 02:01:22 -0700

    Add files via upload

---
+++ hello to client/hello to client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hello_to_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter your name...");
+            var name = Console.ReadLine();
+            Console.WriteLine("hello " + name);
+            Console.WriteLine("press any key to end...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ triple the number/triple the number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace triple_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("hello please enter your number...");
+            string number = Console.ReadLine();
+            x = int.Parse(number);
+            x *= 3;
+            Console.WriteLine(x);
+            Console.ReadKey();
+
+        }
+    }
+}
